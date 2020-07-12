f82daf1fa6bca3131218dd2c1e62078839d47f8f
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-07-13 00:27:55 +0100

    Update exercise-part-0.js
    Week1 ex1 is done

--- Week-1/InClass/A-objects-intro/exercise-part-0.js
+++ Week-1/InClass/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,12 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+
+let myLaptop = {
+    brand: "MacBook",
+    screenSize: 13,
+    hardDisc: 128,
+    MicroChip: "8 Gen I5",
+    operatingSystem: "MacOs",
+    RAM: 8,
+}
