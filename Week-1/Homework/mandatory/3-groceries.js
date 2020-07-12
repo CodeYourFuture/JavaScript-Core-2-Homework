79d014bf4229e3dc33069c63309f9a8bfe56b7d7
carterd888 <62474051+carterd888@users.noreply.github.com>
2020-07-12 17:23:55 +0100

    completed mandatory 3

--- Week-1/Homework/mandatory/3-groceries.js
+++ Week-1/Homework/mandatory/3-groceries.js
@@ -6,7 +6,13 @@
 let groceriesToBuy = [];
 
 let groceryList = {
-  item1: "",
-  item2: "",
-  item3: ""
+  item1: "Potatoes",
+  item2: "Orange Juice",
+  item3: "Rice"
 };
+
+for (let items in groceryList) {
+  groceriesToBuy.push(groceryList[items]);
+};
+
+console.log(groceriesToBuy);
