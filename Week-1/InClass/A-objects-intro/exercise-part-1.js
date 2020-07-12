c75f877638e517cf04750a874f371f54db27e359
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-07-13 00:47:03 +0100

    Update exercise-part-1.js
    a-objects-ex-part1 is done

--- Week-1/InClass/A-objects-intro/exercise-part-1.js
+++ Week-1/InClass/A-objects-intro/exercise-part-1.js
@@ -7,3 +7,46 @@ Assign each of them to a separate variable
 
 */
 
+let myCar ={
+    color: "black",
+    made:"Vauxhall",
+    model:"Insignia",
+    engine:2,
+    productionYear:2015,
+}
+
+let BoschSession1 ={
+    chapter1:"TIS",
+    chapter2:"Lost Night",
+    chapter3:"Blue Religion",
+    chapter4:"Fugazi",
+    chapter5:"Mam's Boy",
+    chapter6:"Donkey's Years",
+    chapter7:"Lost Boys",
+    chapter8:"High Low",
+    chapter9:"The Magic Castle",
+    chapter10:"Us and Them",
+}
+
+let garden ={
+    width:5,
+    length:7,
+    tress: ["Oak", "beech", "ash", "birch"],
+    patio:true,
+}
+
+let livingRoom ={
+    size: "3m * 3m",
+    TVStand:true,
+    window:2,
+    floor: "laminat parke",
+    radiators:3,
+}
+
+let browniCake ={
+    flour: 10,
+    egggs: 2,
+    butter: 15,
+    sugar: 5,
+    cholatte: 10,
+}
