e24511e66ce45e7c8b31d649f9dd2300ff2e4d73
Hiba-moh <Hiba.moh@yahoo.com>
2020-07-13 10:24:06 +0100

    Tests added

--- Week-1/Homework/mandatory/3-groceries.js
+++ Week-1/Homework/mandatory/3-groceries.js
@@ -2,6 +2,13 @@
 // 1. Update your groceryList with the items you need: Potatoes, Orange Juice and Rice.
 // 2. Loop through the groceryList object to gather the item properties into the groceriesToBuy array.
 // 3. Then use console.log() to print out the list. It should print ['Potatoes', 'Orange Juice', 'Rice']
+let log = console.log,
+  logged = [];
+
+console.log = function () {
+  logged.push(arguments[0]);
+  log(...arguments);
+};
 
 let groceriesToBuy = [];
 
@@ -19,3 +26,52 @@ for (let key in groceryList) {
   groceriesToBuy.push (groceryList[key]);
 }
 console.log (groceriesToBuy);
+
+
+
+/* ======= TESTS - DO NOT MODIFY ===== 
+There are some Tests in this file that will help you work out if your code is working.
+To run these tests type `node 3-groceries.js` into your terminal or run in VS code (F5)
+*/
+
+function test(test_name, expr, expected, actual) {
+  let status;
+  if (expr) {
+    status = "PASSED";
+  } else {
+    status = "FAILED";
+  }
+
+  log(
+    `\n${test_name}: ${status}
+    Expected: ${expected}
+    Actual: ${actual}`.replace(/\}/g, "    }") // Format object clearly in output
+  );
+}
+
+const expectedGroceryList = {
+  item1: "Potatoes",
+  item2: "Orange Juice",
+  item3: "Rice",
+};
+
+const expectedGroceriesToBuy = ["Potatoes", "Orange Juice", "Rice"];
+
+test(
+  "Grocery list object has correct property values",
+  JSON.stringify(expectedGroceryList) === JSON.stringify(groceryList),
+  JSON.stringify(expectedGroceryList, null, "\t"),
+  JSON.stringify(groceryList, null, "\t")
+);
+test(
+  "Groceries to buy array has correct values",
+  JSON.stringify(expectedGroceriesToBuy) === JSON.stringify(groceriesToBuy),
+  JSON.stringify(expectedGroceriesToBuy),
+  JSON.stringify(groceriesToBuy)
+);
+test(
+  "Logs groceries to buy array correctly",
+  JSON.stringify(expectedGroceriesToBuy) === JSON.stringify(logged[0]),
+  `Logs ${JSON.stringify(expectedGroceriesToBuy)}`,
+  `Logs ${JSON.stringify(logged[0])}`
+);
