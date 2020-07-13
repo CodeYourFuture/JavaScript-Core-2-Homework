5f2c9c9d0f75404bbb98446fa6edb86fa84f9ad0
Hiba-moh <Hiba.moh@yahoo.com>
2020-07-13 03:33:33 +0100

    Mandatory-3-groceries- Done

--- Week-1/Homework/mandatory/3-groceries.js
+++ Week-1/Homework/mandatory/3-groceries.js
@@ -10,3 +10,12 @@ let groceryList = {
   item2: "",
   item3: ""
 };
+
+groceryList.item1 = "Potatoes";
+groceryList.item2 = "Orange Juice";
+groceryList.item3 = "Rice";
+
+for( let key in groceryList){
+    groceriesToBuy.push(groceryList[key]);
+}
+console.log(groceriesToBuy);
