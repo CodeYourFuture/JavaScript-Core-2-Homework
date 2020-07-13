3feea3d82f187eb52e2e973f2cce79d979a1ef28
murselaysan <64005116+murselaysan@users.noreply.github.com>
2020-07-13 21:34:15 +0100

    Mandatory 3 is ok.
    groceries done

--- Week-1/Homework/mandatory/3-groceries.js
+++ Week-1/Homework/mandatory/3-groceries.js
@@ -3,10 +3,14 @@
 // 2. Loop through the groceryList object to gather the item properties into the groceriesToBuy array.
 // 3. Then use console.log() to print out the list. It should print ['Potatoes', 'Orange Juice', 'Rice']
 
-let groceriesToBuy = [];
+
 
 let groceryList = {
-  item1: "",
-  item2: "",
-  item3: ""
+  item1: "Potatoes",
+  item2: "Orange Juice",
+  item3: "Rice"
 };
+
+let groceriesToBuy = Object.values(groceryList);
+
+console.log(groceriesToBuy)
