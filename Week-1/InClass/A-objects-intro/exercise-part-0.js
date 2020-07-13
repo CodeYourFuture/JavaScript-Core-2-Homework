9770557b0d3b57670d2f61eda27e4e0d006fe5a0
osagiestar <okoedooc@yahoo.com>
2020-07-13 04:19:51 +0100

    object setup

--- Week-1/InClass/A-objects-intro/exercise-part-0.js
+++ Week-1/InClass/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,17 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+
+myLaptop = {
+    make: "Dell",
+    "Operating System": 'Windows 10',
+    storage: "500GB",
+    RAM: '8GB',
+    installed_app: {
+        devApp: 4,
+        windowsApp: 8,
+        googleApp: 7
+    },
+    processor: 'core-i5',
+        ifNew: true
+}
