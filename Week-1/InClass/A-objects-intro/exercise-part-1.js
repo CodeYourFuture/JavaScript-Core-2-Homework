caf5a1e6b6d13d23be5fdbbc59f37512f61946bf
osagiestar <okoedooc@yahoo.com>
2020-07-13 05:34:08 +0100

    setting object from console.log

--- Week-1/InClass/A-objects-intro/exercise-part-1.js
+++ Week-1/InClass/A-objects-intro/exercise-part-1.js
@@ -7,3 +7,36 @@ Assign each of them to a separate variable
 
 */
 
+
+let book = {
+    title: 'Oliver Twist',
+    author: 'Charles Dickins',
+    publishing_year: 1956,
+    no_of_pages: 200,
+    ishardCover: false
+};
+
+let kitten = {
+    fur_colour: "orange",
+    age:  "23"
+};
+
+let television = {
+    brand: "Samsung",
+    isSmartTV: true,
+    screen_size = 40,
+};
+
+let phone = {
+    operating_system: "iOS",
+    hasStylus: true,
+    megapixels: 12,
+    batteryLife: "24 hours"
+};
+
+let car = {
+    make: "Honda",
+    model: 'Civic',
+    manufacture_year: 2020,
+    transmission: automatic
+}
