05d7c9b9c98b067b3b8ccc4c02a57721821ba50c
murselaysan <64005116+murselaysan@users.noreply.github.com>
2020-07-16 12:04:02 +0100

    InClass D is ok.
    methods done

--- Week-1/InClass/D-methods/exercise-5.js
+++ Week-1/InClass/D-methods/exercise-5.js
@@ -16,13 +16,27 @@ let coffeeMachine = {
         flatWhite: 3.00
     },
     insertedAmount: 0,
-    insertMoney: function (amount) {
-
+    insertMoney: function(amount) {
+        this.insertedAmount = amount;
+        if (amount >= 1.50 && amount < 2.40) {
+            return this.insertedAmount;
+        }else if (amount >= 2.40 && amount < 3.00) {
+             return this.insertedAmount;
+        }else if (amount >= 3.00) {
+          return this.insertedAmount;
+        }
     },
     getCoffee: function (coffee) {
-
+        if (this.insertedAmount >= 1.50 && this.insertedAmount < 2.40) {
+            return `Please take your ${coffee}`;
+        }else if (this.insertedAmount >= 2.40 && this.insertedAmount < 3.00) {
+            return `Please take your ${coffee}`;
+        }else if (this.insertedAmount >= 3.00) {
+            return `Please take your ${coffee}`;
+        }
     }
 };
+
 
 
 /*
