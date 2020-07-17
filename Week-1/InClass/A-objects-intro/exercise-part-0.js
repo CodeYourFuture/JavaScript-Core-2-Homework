49772262c52051eefa77f91004490777d4a1cb4b
Gennady Tabala <gtabala@gmail.com>
2020-07-18 00:01:29 +0100

    Final

--- Week-1/InClass/A-objects-intro/exercise-part-0.js
+++ Week-1/InClass/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,16 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+let myLaptop = {
+    barnd: "HP",
+    model: "Probook 650 G1",
+    os: "Windows 10 Pro",
+    processor: "Intel i5",
+    rom: "16 Gb",
+    storage: {
+        brand: "Samsung",
+        model: "860 EVO",
+        type: "SDD",
+        value: "1 Tb"
+    }
+}
--- Week-1/InClass/A-objects-intro/exercise-part-1.js
+++ Week-1/InClass/A-objects-intro/exercise-part-1.js
@@ -6,4 +6,31 @@ can describe with a JavaScript object
 Assign each of them to a separate variable
 
 */
+let compMouse ={
+    type: "Mouse",
+    brend: "Logitech",
+    model: "B605"
+}
 
+let phone = {
+    brend: "Apple",
+    model: "iPhone SE",
+    storage: "128 Gb"
+}
+
+let chair = {
+    material: "wood",
+    legs: 4,
+    back: true
+}
+
+let suitcase = {
+    wheels: true,
+    locks: false,
+    color: "black"
+}
+
+let pen = {
+    fontain: true,
+    color: "violet",
+}
