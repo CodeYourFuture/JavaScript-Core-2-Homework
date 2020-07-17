77ba9c2bb2ebb6fd4478d0d248be53daf4a1edf8
g1st <gintstan@gmail.com>
2020-07-17 07:21:31 +0100

    Mandatory solutions

--- Week-1/Homework/mandatory/2-water-bottle.js
+++ Week-1/Homework/mandatory/2-water-bottle.js
@@ -10,15 +10,18 @@ We made a start on this for you here:
 
 let bottle = {
   volume: 0,
-  fill: function() {
+  fill: function () {
     // calling this function should make you bottles volume = 100;
+    this.volume = 100;
   },
-  drink: function() {
+  drink: function () {
     // calling this function should decrease your bottles volume by 10;
+    this.volume > 0 ? (this.volume -= 10) : null;
   },
-  empty: function() {
+  empty: function () {
     // this function should return true if your bottles volume = 0
-  }
+    return this.volume <= 0;
+  },
 };
 
 /*
@@ -38,4 +41,4 @@ bottle.drink();
 if (!bottle.empty()) {
   console.log(`bottles volume = ${bottle.volume}`);
 }
-console.log("Above volume should be: 70");
+console.log('Above volume should be: 70');
