99d606cecba9a42d23a28a82af8f67792e4f1689
Gennady Tabala <gtabala@gmail.com>
2020-07-18 17:36:53 +0100

    Classwork

--- Week-2/InClass/A-dom-manipulation/exercise.js
+++ Week-2/InClass/A-dom-manipulation/exercise.js
@@ -15,6 +15,10 @@ Write JavaScript below that logs:
     --> should log a list of nodes with a length of 3
 
 */
+console.log(document.getElementsByTagName("p"));
+console.log(document.querySelector(".site-header"));
+console.log(document.querySelector("#jumbotron-text"));
+console.log(document.querySelector(".primary-content p"));
 
 
 /*
@@ -24,22 +28,32 @@ Task 2
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
 
-
+let alertButton = document.getElementById("alertBtn");
+console.log("alertButton");
+alertButton.addEventListener("click", () => alert("Thanks for visiting Bikes for Refugees!"));
 /*
 Task 3
 =======
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
-
+let bgrChangeBtn = document.getElementById("bgrChangeBtn");
+bgrChangeBtn.addEventListener("click",() => document.querySelector("body").style.backgroundColor = "skyblue");
 
 /*
 Task 4
 ======
 
-When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
+When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says ""
 */
+let addTextBtn = document.getElementById("addTextBtn");
+addTextBtn.addEventListener("click",() =>{ 
+  let par =  document.createElement("p");
+  let contex = document.querySelector(".jumbotron");
+  par.textContent = "Read more below.";
+  contex.appendChild(par);
 
+});
 
 
 /*
@@ -48,3 +62,4 @@ Task 5
 
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
+let largerLinksBtn =
