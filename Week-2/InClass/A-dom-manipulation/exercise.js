ac6197414cb01306477cd5155c2e5736ddd45af0
g1st <gintstan@gmail.com>
2020-07-21 13:49:30 +0100

    Add InClass exercises

--- Week-2/InClass/A-dom-manipulation/exercise.js
+++ Week-2/InClass/A-dom-manipulation/exercise.js
@@ -15,15 +15,26 @@ Write JavaScript below that logs:
     --> should log a list of nodes with a length of 3
 
 */
-
-
+const allP = document.querySelectorAll('p');
+console.log(allP);
+const firstDiv = document.querySelector('div');
+console.log(firstDiv);
+const jumbotronTextElement = document.getElementById('jumbotron-text');
+console.log(jumbotronTextElement);
+console.log(document.querySelectorAll('.primary-content p'));
 /*
 Task 2
 ======
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
+
 */
 
+const alertButton = document.querySelector('#alertBtn');
+console.log(alertButton);
+alertButton.addEventListener('click', () =>
+  alert('Thanks for visiting Bikes for Refugees!')
+);
 
 /*
 Task 3
@@ -32,6 +43,19 @@ Task 3
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
 
+const bodyElement = document.querySelector('body');
+// const bodyElement = document.getElementsByTagName('body')[0];
+const changeColourButton = document.querySelector('#bgrChangeBtn');
+// const changeColourButton = document.getElementById('bgrChangeBtn');
+
+const allDivs = document.getElementsByTagName('div');
+console.log(allDivs);
+changeColourButton.addEventListener('click', () => {
+  //   bodyElement.style.backgroundColor = 'orangered';
+  //   console.log(bodyElement);
+  //   console.log(changeColourButton);
+  Array.from(allDivs).forEach((div) => (div.style.backgroundColor = 'orange'));
+});
 
 /*
 Task 4
@@ -39,12 +63,29 @@ Task 4
 
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
+const addTextBtn = document.getElementById('addTextBtn');
 
-
-
+const p = document.createElement('p');
+const context = document.querySelector('.jumbotron');
+addTextBtn.addEventListener('click', () => {
+  p.textContent = 'Read more below.';
+  context.appendChild(p);
+});
 /*
 Task 5
 ======
 
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
+
+const largerLinksBtn = document.getElementById('largerLinksBtn');
+
+largerLinksBtn.addEventListener('click', () => {
+  const allLinks = document.querySelectorAll('a');
+
+  allLinks.forEach((anchor) => {
+    let currentSize = anchor.style.fontSize; // 1em
+    let justSizeNumber = parseFloat(currentSize);
+    anchor.style.fontSize = justSizeNumber * 1.1 + 'rem';
+  });
+});
