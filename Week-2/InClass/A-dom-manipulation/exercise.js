2df59d56553177570d8a5c6b7f8288619516f429
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-07-21 01:02:12 +0100

    inClass exercise.js is done

--- Week-2/InClass/A-dom-manipulation/exercise.js
+++ Week-2/InClass/A-dom-manipulation/exercise.js
@@ -18,6 +18,17 @@ Write JavaScript below that logs:
 let allPElements = document.querySelectorAll('p');
 console.log(allPElements);
 
+let firstDivElement = document.querySelector(".site-header.container");
+console.log(firstDivElement);
+
+let jumbotronT = document.querySelector("#jumbotron-text");
+
+console.log(jumbotronT);
+
+let primaryContentP= document.querySelectorAll(".primary-content p");
+
+console.log(primaryContentP);
+
 /*
 Task 2
 ======
@@ -25,6 +36,12 @@ Task 2
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
 
+ let myButton = document.querySelector("#alertBtn");
+myButton.addEventListener("click", alertSomething);
+
+function alertSomething() {
+  alert("Thanks for visiting Bikes for Refugees!");
+}
 
 /*
 Task 3
@@ -33,6 +50,13 @@ Task 3
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
 
+let myBackgroundColor = document.getElementById("bgrChangeBtn");
+myBackgroundColor.addEventListener("click", changeBackGroundColor);
+
+function changeBackGroundColor() {
+    let selectBodyElement= document.querySelector("body");
+    selectBodyElement.style.backgroundColor= "red";
+}
 
 /*
 Task 4
@@ -41,7 +65,15 @@ Task 4
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
 
+let addNewParagraph = document.querySelector("#addTextBtn");
+addNewParagraph.addEventListener("click", addSomeMoreText);
 
+function addSomeMoreText() {
+  let paragraph = document.createElement("p");
+    paragraph.textContent = "Read more below."; 
+  let buttonSame = document.querySelector(".buttons")
+  buttonSame.appendChild(paragraph);
+}
 
 /*
 Task 5
@@ -49,3 +81,23 @@ Task 5
 
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
+
+let linksPage = document.querySelector("#largerLinksBtn");
+linksPage.addEventListener("click", textSizeGetBigger);
+
+function textSizeGetBigger() {
+  let allLinksOnThePage = document.getElementsByTagName("a");
+
+ for (var i=0; i < allLinksOnThePage.length; i++ ){
+     allLinksOnThePage[i].classList.add(".larger");
+ }
+}
+
+/* Wahab lecture example
+let btn = document.querySelector('#largerLinksBtn');
+let allLinks = document.querySelectorAll('a');
+btn.addEventListener("click", function(){
+for(let i = 0; i < allLinks.length; i++){
+allLinks[i].classList.add("larger");
+}
+}); */
