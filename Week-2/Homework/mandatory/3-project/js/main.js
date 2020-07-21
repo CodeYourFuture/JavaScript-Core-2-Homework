01b94adabec4033981479080752a3dab8f9fa428
g1st <gintstan@gmail.com>
2020-07-21 16:01:39 +0100

    Add mandatory project part 2

--- Week-2/Homework/mandatory/3-project/js/main.js
+++ Week-2/Homework/mandatory/3-project/js/main.js
@@ -1,3 +1,5 @@
+// Part 1
+
 const blueBtn = document.getElementById('blueBtn');
 const orangeBtn = document.getElementById('orangeBtn');
 const greenBtn = document.getElementById('greenBtn');
@@ -30,3 +32,38 @@ function handleGreenButton() {
 blueBtn.addEventListener('click', handleBlueButton);
 orangeBtn.addEventListener('click', handleOrangeButton);
 greenBtn.addEventListener('click', handleGreenButton);
+
+// Part 2
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const inputs = form.querySelectorAll('.primary-content .form-control');
+
+  // returns true when there are no errors
+  if (validateInputs(inputs)) {
+    inputs.forEach((input) => (input.value = ''));
+    alert('Thank you for filling out the form');
+  }
+});
+
+function validateInputs(inputs) {
+  let formValid = true;
+
+  inputs.forEach((input) => {
+    if (
+      input.value.length < 1 ||
+      (input.type === 'email' && !input.value.includes('@'))
+    ) {
+      input.style.backgroundColor = 'red';
+      formValid = false;
+    } else {
+      input.style.backgroundColor = 'white';
+    }
+  });
+
+
+  return formValid;
+}
