a981988fea5202ef194cd2cb7bbc3dedece37317
davibaweja <davibaweja@gmail.com>
2020-07-22 19:18:15 +0100

    V3.2 week2
    Completed the part 1 with three buttons, blue orange and green.

--- Week-2/Homework/mandatory/3-project/js/main.js
+++ Week-2/Homework/mandatory/3-project/js/main.js
@@ -1,26 +1,41 @@
 
 var bluebutton = document.getElementById("blueBtn")
+var orangeButton=document.getElementById("orangeBtn");
+var greenButton=document.getElementById("greenBtn");
 
-function clickBlueButton()
+function clickButton(jumboColor,donateColor,volunteerColor1,volunteerColor2)
 {
     var jumbotron=document.querySelector(".jumbotron");
-    jumbotron.style.backgroundColor=`#588fbd`;
+    jumbotron.style.backgroundColor=jumboColor;
 
     var buttons=document.querySelector(".buttons");
     var donateBike=buttons.firstElementChild;
     var Volunteer=buttons.lastElementChild;
 
-    donateBike.style.backgroundColor=`#ffa500`;
-    Volunteer.style.backgroundColor="black";
-    Volunteer.style.color=`white`;
-
+    donateBike.style.backgroundColor=donateColor;
+    Volunteer.style.backgroundColor=volunteerColor1;
+    Volunteer.style.color=volunteerColor2;
 
 }
 
-bluebutton.addEventListener("click",clickBlueButton)
 
+function buttonForBlue()
+{
+    clickButton(`#588fbd`,`#ffa500`,"black",`white`);
+}
 
+function buttonforOrange()
+{
+    clickButton(`#f0ad4e`,`#5751fd`,`#31b0d5`,`white`);
+}
 
+function buttonForGreen()
+{
+    clickButton(`#87ca8a`,`black`,`#8c9c08`,`white`);
+}
 
+bluebutton.addEventListener("click",buttonForBlue);
 
+orangeButton.addEventListener("click",buttonforOrange);
 
+greenButton.addEventListener("click",buttonForGreen);
