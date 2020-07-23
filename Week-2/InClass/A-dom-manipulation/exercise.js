57bceeeb10163a3177989ea0b7889cf2d4f4dca7
iggajewski <64140239+iggajewski@users.noreply.github.com>
2020-07-23 14:01:00 +0100

    in-class update

--- Week-2/InClass/A-dom-manipulation/exercise.js
+++ Week-2/InClass/A-dom-manipulation/exercise.js
@@ -15,7 +15,15 @@ Write JavaScript below that logs:
     --> should log a list of nodes with a length of 3
 
 */
+let paragraphs = document.querySelectorAll("p");
+let firstDiv = document.querySelector(".side-header")
+let jumbotron = document.querySelector("#jumbotron-text")
+let primaryContent = document.querySelector(".primary-content p")
 
+//console.log(paragraphs);
+//console.log(firstDiv);
+//console.log(jumbotron);
+//console.log(primaryContent);
 
 /*
 Task 2
@@ -32,6 +40,11 @@ Task 3
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
 
+let changeColorButton = document.querySelector("#bgrChangeBtn");
+
+changeColorButton.addEventListener("click", function() {
+    document.body.style.backgroundColor = "green";
+})
 
 /*
 Task 4
@@ -40,7 +53,14 @@ Task 4
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
 
+let addTextBtn = document.querySelector("#addTextBtn");
+let paragraph = document.createElement("p");
 
+addTextBtn.addEventListener("click", function() {
+    let element = document.querySelector(".heading-underline");
+    paragraph.textContent = "READ MORE BELOW";
+    element.appendChild(paragraph);
+});
 
 /*
 Task 5
@@ -48,3 +68,12 @@ Task 5
 
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
+// CSS style has to be added for .larger
+let largerText = document.querySelector("#largerLinksBtn");
+let links = document.querySelectorAll("a");
+
+largerText.addEventListener("click", function() {
+    for (let i = 0; i < links.length; ++i) {
+        links[i].classList.add("larger");
+    }
+});
