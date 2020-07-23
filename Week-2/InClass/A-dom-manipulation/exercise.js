3327514885bdddff625e3caefa649bf84cc20161
zubeda <zubedauk@gmail.com>
2020-07-23 11:32:14 +0100

    InClass

--- Week-2/InClass/A-dom-manipulation/exercise.js
+++ Week-2/InClass/A-dom-manipulation/exercise.js
@@ -4,47 +4,76 @@ Task 1
 Write JavaScript below that logs:
     1. all the "p" element nodes of the document,
     --> should log a list of nodes with a length of 6
-
     2. the first div element node
     --> should log the ".site-header" node
-
     3. the element with id "jumbotron-text"
     --> should log the "#jumbotron-text" node
-
-    4. all the "p" elements of contained inside  the .primary-content element node
+    4. all the "p" elements of contained inside  
+    the .primary-content element node
     --> should log a list of nodes with a length of 3
-
 */
-
+//1
+let pElements=document.getElementsByTagName("p");
+console.log(pElements);
+//2
+let firstDiv=document.getElementsByTagName("div");
+console.log(firstDiv[0]);
+//3
+let jumbo=document.getElementById("jumbotron-text")
+console.log(jumbo);
+//4
+let primarycontent=document.querySelector(".primary-content");
+//let para=document.getElementsByTagName("p");
+console.log(primarycontent);
+//console.log(document.para.pElements(primarycontent));
 
 /*
 Task 2
 ======
-
-When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
+When a user clicks the 'ALERT' button, an alert box should pop up with the text
+ "Thanks for visiting Bikes for Refugees!"
 */
-
-
+var clickAlert=document.getElementById("alertBtn");
+clickAlert.addEventListener("click",function(event){
+    alert("Thanks for visiting Bikes for Refugees!");
+})
 /*
 Task 3
 =======
-
-Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
+Write JavaScript below that changes the background colour of the page
+ when the 'Change colour' button is clicked.
 */
-
+var bgrChangeBtn=document.getElementById("bgrChangeBtn");
+let color=["red","green","blue"]
+bgrChangeBtn.addEventListener("click",function(){
+    document.querySelector("body").style.backgroundColor
+    =color[Math.floor(Math.random()*color.length)];
+})
 
 /*
 Task 4
 ======
-
-When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
+When a user clicks the 'Add some text' button, a new paragraph 
+should be added below the buttons that says "Read more below."
 */
+let addBtn=document.getElementById("addTextBtn");
 
-
+addBtn.addEventListener("click",function(event){
+    let para=document.createElement("p");
+    para.textContent="Read more below";
+    addBtn.appendChild(para);
+})
 
 /*
 Task 5
 ======
-
-When the 'Larger links!' button is clicked, the text of all links on the page should increase.
+When the 'Larger links!' button is clicked, 
+the text of all links on the page should increase.
 */
+let largerLinksBtn=document.getElementById("largerLinksBtn");
+let links=document.getElementsByTagName("a");
+largerLinksBtn.addEventListener("click",function(event){
+    for(let link of links){
+     link.style.fontSize="30px";
+    }
+})
