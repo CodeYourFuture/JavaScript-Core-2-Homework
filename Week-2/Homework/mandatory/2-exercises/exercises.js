935924035ae47221210a9c1ebf9ab56378378055
iggajewski <64140239+iggajewski@users.noreply.github.com>
2020-07-24 18:10:08 +0100

    completed exercises (.../mandatory/2-exercises)

--- Week-2/Homework/mandatory/2-exercises/exercises.js
+++ Week-2/Homework/mandatory/2-exercises/exercises.js
@@ -15,6 +15,19 @@
  */
 function exerciseOne(arrayOfPeople) {
   let content = document.querySelector("#content");
+
+  for(let i = 0; i < arrayOfPeople.length; ++i) {
+    let personDiv = document.createElement("div");
+    content.appendChild(personDiv);
+
+    let nameEl = document.createElement("h1");
+    nameEl.innerHTML = arrayOfPeople[i].name;
+    personDiv.appendChild(nameEl);
+
+    let jobEl = document.createElement("h2");
+    jobEl.innerHTML = arrayOfPeople[i].job;
+    personDiv.appendChild(jobEl);
+  }
 }
 
 /**
@@ -25,7 +38,20 @@ function exerciseOne(arrayOfPeople) {
  *
  */
 function exerciseTwo(shopping) {
-  //Write your code in here
+  let content = document.querySelector("#content");
+
+  let titleEl = document.createElement("h3");
+  titleEl.innerHTML = "<u>shopping list:</u>";
+  content.appendChild(titleEl);
+
+  let listEl = document.createElement("ul");
+  content.appendChild(listEl);
+
+  for(let i = 0; i < shopping.length; ++i) {
+    let itemEl = document.createElement("li");
+    itemEl.innerHTML = shopping[i];
+    listEl.appendChild(itemEl);
+  }
 }
 
 /**
@@ -57,8 +83,50 @@ function exerciseTwo(shopping) {
 
     The end result should look something like this: https://hyf-js2-week1-makeme-ex1-demo.herokuapp.com/
 **/
+
+
 function exerciseThree(books) {
-  //Write your code in here
+  let content = document.querySelector("#content");
+
+  let titleEl = document.createElement("h3");
+  titleEl.innerHTML = "<u>List of favorite books:</u>";
+  content.appendChild(titleEl);
+
+  let booksDiv = document.createElement("div");
+  booksDiv.style.display = "flex";
+  content.appendChild(booksDiv);
+
+  for(let i = 0; i < books.length; ++i) {
+    let bookEl = document.createElement("div");
+    bookEl.style.padding = "20px";
+    bookEl.style.margin = "0 20px 0 20px";
+    booksDiv.appendChild(bookEl);
+
+    if(books[i].alreadyRead) {
+      bookEl.style.backgroundColor = "green";
+    }
+    else {
+      bookEl.style.backgroundColor = "red";
+    }
+
+    let bookTitleEl = document.createElement("p");
+    bookTitleEl.innerHTML = books[i].title + " - " + books[i].author;
+    bookEl.appendChild(bookTitleEl);
+
+    let bookImageEl = document.createElement("img");
+    // It would be less messy if I could modify the object array or if the images were already provided
+    if(books[i].title === "The Design of Everyday Things") {
+      bookImageEl.src = "https://assets.wired.com/photos/w_1001/wp-content/uploads/2015/09/design-of-everyday-things.jpg";
+    }
+    if(books[i].title === "The Most Human Human") {
+      bookImageEl.src = "https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg";
+    }
+    if(books[i].title === "The Pragmatic Programmer") {
+      bookImageEl.src = "https://upload.wikimedia.org/wikipedia/en/8/8f/The_pragmatic_programmer.jpg";
+    }
+    bookImageEl.height = 256;
+    bookEl.appendChild(bookImageEl);
+  }
 }
 
 //
