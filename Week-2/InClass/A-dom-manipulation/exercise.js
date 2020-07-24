e3666b9556a1018d2d81d0dbf06e2e72884537c7
Ade0211 <62720742+Ade0211@users.noreply.github.com>
2020-07-24 14:27:51 +0100

    mandatory
    I answered my mandatory questions

--- Week-2/InClass/A-dom-manipulation/exercise.js
+++ Week-2/InClass/A-dom-manipulation/exercise.js
@@ -15,7 +15,17 @@ Write JavaScript below that logs:
     --> should log a list of nodes with a length of 3
 
 */
+let allPElements = document.querySelectorAll("p");
+console.log(allPElements);
 
+let firstDivEl = document.querySelector("div");
+console.log(firstDivEl);
+
+let idJumbotronText = document.querySelector("#jumbotron-text");
+console.log(idJumbotronText);
+
+let allPElementsInPrimaryContent = document.querySelectorAll(".primary-content p");
+console.log(allPElementsInPrimaryContent);
 
 /*
 Task 2
@@ -23,7 +33,10 @@ Task 2
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
-
+let alertBtn = document.querySelector("#alertBtn");
+alertBtn.addEventListener("click", function(){
+    alert("Thanks for visiting Bikes for Refugees!");
+});
 
 /*
 Task 3
@@ -31,7 +44,10 @@ Task 3
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
-
+let changeColorBtn = document.querySelector("#bgrChangeBtn");
+changeColorBtn.addEventListener("click", function(){
+    changeColorBtn.style.backgroundColor = " yellow";
+});
 
 /*
 Task 4
@@ -39,7 +55,12 @@ Task 4
 
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
-
+let addTextBtn = document.querySelector("#addTextBtn");
+addTextBtn.addEventListener("click", function() {
+    let addParagraph = document.createElement("p");
+    addParagraph.textContent = "Read more below."
+    addTextBtn.appendChild(addParagraph);
+});
 
 
 /*
@@ -48,3 +69,11 @@ Task 5
 
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
+
+
+
+let largerButton = document.querySelector("#largerLinksBtn");
+largerButton.addEventListener("click", function(){
+let allLinks = document.querySelectorAll("a");
+  allLinks.forEach(btn => btn.style.fontSize = "1.5rem")
+});
