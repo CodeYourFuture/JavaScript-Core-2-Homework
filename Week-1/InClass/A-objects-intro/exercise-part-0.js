c5d3cb838b13737e0934ba8eb890b563b3348ab8
Ali Haider <alihaiderx736@gmail.com>
2020-07-26 12:50:48 +0100

    In Class A-Objects-Intro exercise Completed.

--- Week-1/InClass/A-objects-intro/exercise-part-0.js
+++ Week-1/InClass/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,13 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+
+let laptop = {
+    brand : "HP",
+    screenSize : 15,
+    isTouchscreen : false,
+    ramMemorysize : 8,
+    storageMemorysize : 250
+};
+
+console.log(laptop);
