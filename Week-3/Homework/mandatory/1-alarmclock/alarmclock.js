63251af9d3060e4b86e0a2622024a81e47d40aec
g1st <gintstan@gmail.com>
2020-07-26 13:14:21 +0100

    Add alarmclock extra tasks

--- Week-3/Homework/mandatory/1-alarmclock/alarmclock.js
+++ Week-3/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,35 +1,90 @@
 let alarmTimer;
+let blinkingScreenTimer;
+let alarmRunning = false;
+let seconds;
+let h1;
 
 function setAlarm() {
-  let seconds = document.getElementById('alarmSet').value;
-  const h1 = document.getElementById('timeRemaining');
+  // stop alarm if currently running
+  stopAlarm();
+
+  seconds = document.getElementById('alarmSet').value;
+  h1 = document.getElementById('timeRemaining');
+  alarmRunning = true;
+
+  // can't set alarm for 0 or less seconds
+  if (seconds < 1) return;
 
   // call function without waiting 1000ms for the first call of setInterval
   updateTime(seconds, h1);
 
-  // clear previous timer
-  clearInterval(alarmTimer);
-
   alarmTimer = setInterval(() => {
     updateTime(seconds, h1);
   }, 1000);
+}
 
-  function updateTime(s, node) {
-    if (s < 1) {
-      clearInterval(alarmTimer);
-      playAlarm();
-    }
-    const time = formatTime(s);
-    node.textContent = `Time Remaining: ${time}`;
-    seconds--;
+function stopAlarm() {
+  alarmRunning = false;
+  clearInterval(blinkingScreenTimer);
+  clearInterval(alarmTimer);
+  audio.pause();
+  seconds = 0;
+  document.body.style.backgroundColor = 'white';
+  const h1 = document.getElementById('timeRemaining');
+  h1.textContent = `Time Remaining: 00:00`;
+
+  document.getElementById('pause').textContent = 'Pause Alarm';
+}
+
+function pauseAlarm() {
+  // ignore pause button when timer is expired or not set
+  if (seconds < 0 || seconds === undefined) return;
+
+  const pauseBtn = document.getElementById('pause');
+
+  pauseBtn.textContent = alarmRunning ? 'Resume Alarm' : 'Pause Alarm';
+
+  if (alarmRunning) {
+    // pause timer
+    clearInterval(alarmTimer);
+  } else {
+    // start timer
+
+    alarmTimer = setInterval(() => {
+      updateTime(seconds, h1);
+    }, 1000);
   }
 
-  function formatTime(seconds) {
-    const s = seconds % 60;
-    const m = Math.floor(seconds / 60);
+  alarmRunning = !alarmRunning;
+}
 
-    return ` ${m < 10 ? `0${m}` : m}:${s < 10 ? `0${s}` : s}`;
+function updateTime(s, node) {
+  const time = formatTime(s);
+  node.textContent = `Time Remaining: ${time}`;
+  seconds--;
+
+  if (s < 1) {
+    startAlarm();
+    clearInterval(alarmTimer);
   }
+}
+
+function formatTime(seconds) {
+  const s = seconds % 60;
+  const m = Math.floor(seconds / 60);
+
+  // 1min and 2s will be 01:02 instead of 1:2
+  return ` ${m < 10 ? `0${m}` : m}:${s < 10 ? `0${s}` : s}`;
+}
+
+function startAlarm() {
+  let counter = 0;
+
+  blinkingScreenTimer = setInterval(() => {
+    playAlarm();
+    document.body.style.backgroundColor = counter % 2 === 0 ? 'red' : 'white';
+    counter++;
+  }, 500);
 }
 
 // DO NOT EDIT BELOW HERE
@@ -42,16 +97,16 @@ function setup() {
   });
 
   document.getElementById('stop').addEventListener('click', () => {
+    stopAlarm();
+  });
+
+  document.getElementById('pause').addEventListener('click', () => {
     pauseAlarm();
   });
 }
 
 function playAlarm() {
   audio.play();
-}
-
-function pauseAlarm() {
-  audio.pause();
 }
 
 window.onload = setup;
