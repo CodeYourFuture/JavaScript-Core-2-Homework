7800d9d36ff3395f7652d8a9ab2b291db6d0b648
Sadat <sadat0akhtar@gmail.com>
2020-07-27 14:40:41 +0100

    completed the clock exercise

--- Week-3/Homework/mandatory/1-alarmclock/alarmclock.js
+++ Week-3/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,45 @@
-function setAlarm() {}
+//Selectors
+
+
+//event listeners
+
+ 
+
+
+function setAlarm() {
+  //SELECTORS
+  let timerValue = document.getElementById('alarmSet');
+  let timeRemaining = document.querySelector('#timeRemaining');
+  let stop = document.querySelector('#stop');
+  let timer =  timerValue.value;
+
+
+    let startCounter = setInterval(countDown, 1000);
+
+    //FUNCTION TO PRINT TIME REMAINING TO HTML
+    function countDown(){
+      
+      if(timer < 0){
+        playAlarm();
+      }else{
+        timeRemaining.textContent = ` Time Remaining: 00:0${timer}`;
+        timer--;
+      }
+    } 
+
+    
+
+
+    //STOPS COUNTDOWN TIMER AND STOPS ALARM
+    stop.addEventListener('click', () =>{
+      clearInterval(startCounter);
+      pauseAlarm();
+      
+    });
+
+}
+
+
 
 // DO NOT EDIT BELOW HERE
 
