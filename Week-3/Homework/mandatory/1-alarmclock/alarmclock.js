0e7de6c8e6b26404658e863b49d2165662724d4d
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-07-28 15:29:53 +0100

    alarmclock is revised

--- Week-3/Homework/mandatory/1-alarmclock/alarmclock.js
+++ Week-3/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,8 +1,38 @@
-function setAlarm() {}
+// Step-1
+function setAlarm(number) {
+  let inputAlarmSet = document.getElementById("alarmSet");
+  inputAlarmSet.textContent = number;
+}
+
+let alarmSetting = document.getElementById("set");
+alarmSetting.addEventListener("click", setAlarm);
+
+//Step-2
+let titleTimeRemaining = document.getElementById("timeRemaining");
+titleTimeRemaining.textContent = `Time Remaining: ${inputAlarmSet.textContent}`;
+
+// Step-3 and Step4
+  setInterval(function(){
+    var audio = new Audio('alarmsound.mp3');
+    let secondClock = inputAlarmSet.textContent;
+
+    if (secondClock > 0){
+      titleTimeRemaining.textContent = `Time Remaining: ${secondClock}`;
+    } else if (secondClock === 0){
+      audio.play();
+      return `Alarm time is elapsed.`;
+    }
+  secondClock --;
+  }, 1000)
+
+// Step-4
 
 
+ 
+  
 
 
+//////////////////////////////////////////////////////////////////////////////////////////////
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
