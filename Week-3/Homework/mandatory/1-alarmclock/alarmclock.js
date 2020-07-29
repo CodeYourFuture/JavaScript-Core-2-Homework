3c99b5c0e715fb8f67a31b1d42aab7177e3d628f
Denis-Andrei <denispeptanariu@gmail.com>
2020-07-29 22:03:10 +0100

    adding 1st exercise COMPLETED

--- Week-3/Homework/mandatory/1-alarmclock/alarmclock.js
+++ Week-3/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,38 @@
-function setAlarm() {}
+
+
+function setAlarm() {
+  let inputValue = document.getElementById('alarmSet').value;
+  let heading = document.getElementById('timeRemaining');
+  let minutes = Math.floor(inputValue / 60);
+  let seconds = (inputValue % 60);
+  
+  let colors = ['#FCF2F1','#E0C1E0','#A88C9B','#A86DB1','#7649CA','#FCF2F1','#E0C1E0','#A88C9B'];
+  
+  let interval = setInterval(() => {
+    
+    heading.textContent = `Time Remaining: ${minutes}:${seconds}`;
+    if(seconds === 0) {
+      seconds = 60;
+      minutes--;
+    }
+    
+    seconds--;
+
+    if(minutes < 0 ){
+      playAlarm(); 
+      clearInterval(interval);
+      
+      setInterval(() => {
+        let random = Math.floor(Math.random() * colors.length);
+        document.body.style.backgroundColor = colors[random];
+      }, 500 )
+      
+    } 
+    
+  }, 1000)
+  
+  
+}
 
 // DO NOT EDIT BELOW HERE
 
