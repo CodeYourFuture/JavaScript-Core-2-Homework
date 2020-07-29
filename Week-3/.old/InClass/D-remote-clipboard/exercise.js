e7fd5e82f3b3e8b4d744ef6af99575546ede3895
murselaysan <64005116+murselaysan@users.noreply.github.com>
2020-07-29 11:56:14 +0100

    .old/D/remote -clipboard has done
    remote clipboard completed

--- Week-3/.old/InClass/D-remote-clipboard/exercise.js
+++ Week-3/.old/InClass/D-remote-clipboard/exercise.js
@@ -20,8 +20,8 @@ Also, for GET request, you can use the url directly in your browser address bar
 // Task 1: create a new clipboard
 // Complete the code below
 
-var clipboardTitle = "CHANGE ME";
-var clipboardText = "CHANGE ME";
+var clipboardTitle = "Birmingham";
+var clipboardText = "I am from Birminghammmm";
 var requestBody = { title: clipboardTitle, text: clipboardText };
 
 var postRequestParameters = {
@@ -32,12 +32,14 @@ var postRequestParameters = {
     }
 };
 
-fetch(/* Write the API address here */, postRequestParameters);
+fetch('https://codeyourfuture.herokuapp.com/api/clipboard', postRequestParameters);
 
 
 // Task 2: Load an existing clipboard
 // Add your code below
 
-fetch(/* ... */).then(function(response) {
+fetch('https://codeyourfuture.herokuapp.com/api/clipboard?title=Birmingham').then(function(response) {
     return response.text();
-}).then(/* ... */);
+}).then(function(result){
+    console.log(result)
+});
