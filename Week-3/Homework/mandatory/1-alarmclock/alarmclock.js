4466e6473ad5235a3e37f2ecea19355e92b8b096
Hiba-moh <Hiba.moh@yahoo.com>
2020-07-29 07:46:35 +0100

    Mandatory-1-alarmclock- Done

--- Week-3/Homework/mandatory/1-alarmclock/alarmclock.js
+++ Week-3/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,25 +1,108 @@
-function setAlarm() {}
+function setAlarm () {
+  let setBtn = document.querySelector ('#set');
+  setBtn.addEventListener ('click', function () {
+    let timeRem = document.querySelector ('#timeRemaining');
+    let inputTxt = document.querySelector ('#alarmSet').value;
+    let intTxt = parseInt (inputTxt);
+
+    if (intTxt > 0 && intTxt < 10) {
+      timeRem.textContent = `Time Remaining: 00:0${inputTxt}`;
+      let interv = setInterval (function () {
+        if (inputTxt > 0)
+          timeRem.textContent = `Time Remaining: 00:0${--inputTxt}`;
+        if (inputTxt === 0) {
+          timeRem.textContent = `Time Remaining: 00:00`;
+          playAlarm ();
+          clearInterval (interv);
+        }
+      }, 1000);
+    }
+
+    if (intTxt >= 10 && intTxt < 60) {
+      timeRem.textContent = `Time Remaining: 00:${inputTxt}`;
+      let interv = setInterval (function () {
+        if (inputTxt > 10) {
+          timeRem.textContent = `Time Remaining: 00:${--inputTxt}`;
+        } else {
+          timeRem.textContent = `Time Remaining: 00:0${--inputTxt}`;
+        }
+        if (inputTxt === 0) {
+          timeRem.textContent = `Time Remaining: 00:00`;
+          playAlarm ();
+          clearInterval (interv);
+        }
+      }, 1000);
+    }
+
+    if (intTxt >= 60 && intTxt < 600) {
+      let mins = Math.floor (intTxt / 60);
+      let sec = intTxt - mins * 60;
+      timeRem.textContent = `Time Remaining: 0${mins}:0${sec}`;
+      let interv = setInterval (function () {
+        if (sec > 0 && sec < 10) {
+          if (sec > 0)
+            timeRem.textContent = `Time Remaining: 0${mins}:0${--sec}`;
+          if (sec === 0) {
+            if (mins > 0) {
+              mins--;
+              sec += 60;
+            }
+            timeRem.textContent = `Time Remaining: 0${mins}:0${sec}`;
+          }
+        }
+        if (sec >= 10 && sec <= 60) {
+          timeRem.textContent = `Time Remaining: 0${mins}:${--sec}`;
+        }
+      }, 1000);
+    }
+
+    if (intTxt > 600 && intTxt < 6000) {
+      let mins = Math.floor (intTxt / 60);
+      let sec = intTxt - mins * 60;
+      let interv = setInterval (function () {
+        if (sec > 0 && sec < 10) {
+          if (sec > 0)
+            timeRem.textContent = `Time Remaining: ${mins}:0${--sec}`;
+          if (sec === 0) {
+            if (mins > 0) {
+              mins--;
+              sec += 60;
+            }
+            timeRem.textContent = `Time Remaining: 0${mins}:0${sec}`;
+          }
+        }
+        if (sec >= 10 && sec <= 60) {
+          timeRem.textContent = `Time Remaining: 0${mins}:${--sec}`;
+        }
+      }, 1000);
+    }
+
+    if (intTxt > 6000) {
+      timeRem.textContent = ` Sorry Its Is Too Big, Follow Up Our Next Version 😉`;
+    }
+  });
+}
 
 // DO NOT EDIT BELOW HERE
 
-var audio = new Audio("alarmsound.mp3");
+var audio = new Audio ('alarmsound.mp3');
 
-function setup() {
-  document.getElementById("set").addEventListener("click", () => {
-    setAlarm();
+function setup () {
+  document.getElementById ('set').addEventListener ('click', () => {
+    setAlarm ();
   });
 
-  document.getElementById("stop").addEventListener("click", () => {
-    pauseAlarm();
+  document.getElementById ('stop').addEventListener ('click', () => {
+    pauseAlarm ();
   });
 }
 
-function playAlarm() {
-  audio.play();
+function playAlarm () {
+  audio.play ();
 }
 
-function pauseAlarm() {
-  audio.pause();
+function pauseAlarm () {
+  audio.pause ();
 }
 
 window.onload = setup;
