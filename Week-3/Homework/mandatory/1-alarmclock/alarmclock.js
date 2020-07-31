11649d416772793eafb91f4438a178127a3d19fa
Hiba-moh <Hiba.moh@yahoo.com>
2020-07-31 05:37:44 +0100

    Fixed an issue
    I had to click set alarm button twice for my event listener code to start to work.
    I realised that I've used new event listener while there is already one in the setup function .. silly me :relieved:

--- Week-3/Homework/mandatory/1-alarmclock/alarmclock.js
+++ Week-3/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,89 +1,85 @@
 function setAlarm () {
   let setBtn = document.querySelector ('#set');
-  setBtn.addEventListener ('click', function () {
-    let timeRem = document.querySelector ('#timeRemaining');
-    let inputTxt = document.querySelector ('#alarmSet').value;
-    let intTxt = parseInt (inputTxt);
-    let interv;
-    let mins;
-    let sec;
+  let timeRem = document.querySelector ('#timeRemaining');
+  let inputTxt = document.querySelector ('#alarmSet').value;
+  let intTxt = parseInt (inputTxt);
+  let interv;
+  let mins;
+  let sec;
+  console.log (inputTxt);
+  if (intTxt > 0 && intTxt < 10) {
+    timeRem.textContent = `Time Remaining: 00:0${inputTxt}`;
+    interv = setInterval (function () {
+      if (inputTxt > 0)
+        timeRem.textContent = `Time Remaining: 00:0${--inputTxt}`;
+      if (inputTxt === 0) {
+        timeRem.textContent = `Time Remaining: 00:00`;
+        playAlarm ();
+        clearInterval (interv);
+      }
+    }, 1000);
+  }
 
-    if (intTxt > 0 && intTxt < 10) {
-      timeRem.textContent = `Time Remaining: 00:0${inputTxt}`;
-      interv = setInterval (function () {
-        if (inputTxt > 0)
-          timeRem.textContent = `Time Remaining: 00:0${--inputTxt}`;
-        if (inputTxt === 0) {
-          timeRem.textContent = `Time Remaining: 00:00`;
-          playAlarm ();
-          clearInterval (interv);
-        }
-      }, 1000);
-    }
+  if (intTxt >= 10 && intTxt < 60) {
+    timeRem.textContent = `Time Remaining: 00:${inputTxt}`;
+    interv = setInterval (function () {
+      if (inputTxt > 10) {
+        timeRem.textContent = `Time Remaining: 00:${--inputTxt}`;
+      } else {
+        timeRem.textContent = `Time Remaining: 00:0${--inputTxt}`;
+      }
+      if (inputTxt === 0) {
+        timeRem.textContent = `Time Remaining: 00:00`;
+        playAlarm ();
+        clearInterval (interv);
+      }
+    }, 1000);
+  }
 
-    if (intTxt >= 10 && intTxt < 60) {
-      timeRem.textContent = `Time Remaining: 00:${inputTxt}`;
-      interv = setInterval (function () {
-        if (inputTxt > 10) {
-          timeRem.textContent = `Time Remaining: 00:${--inputTxt}`;
-        } else {
-          timeRem.textContent = `Time Remaining: 00:0${--inputTxt}`;
-        }
-        if (inputTxt === 0) {
-          timeRem.textContent = `Time Remaining: 00:00`;
-          playAlarm ();
-          clearInterval (interv);
-        }
-      }, 1000);
-    }
-
-    if (intTxt >= 60 && intTxt < 600) {
-      mins = Math.floor (intTxt / 60);
-      sec = intTxt - mins * 60;
-      timeRem.textContent = `Time Remaining: 0${mins}:0${sec}`;
-      interv = setInterval (function () {
-        if (sec > 0 && sec < 10) {
-          if (sec > 0)
-            timeRem.textContent = `Time Remaining: 0${mins}:0${--sec}`;
-          if (sec === 0) {
-            if (mins > 0) {
-              mins--;
-              sec += 60;
-            }
-            timeRem.textContent = `Time Remaining: 0${mins}:0${sec}`;
+  if (intTxt >= 60 && intTxt < 600) {
+    mins = Math.floor (intTxt / 60);
+    sec = intTxt - mins * 60;
+    timeRem.textContent = `Time Remaining: 0${mins}:0${sec}`;
+    interv = setInterval (function () {
+      if (sec > 0 && sec < 10) {
+        if (sec > 0) timeRem.textContent = `Time Remaining: 0${mins}:0${--sec}`;
+        if (sec === 0) {
+          if (mins > 0) {
+            mins--;
+            sec += 60;
           }
+          timeRem.textContent = `Time Remaining: 0${mins}:0${sec}`;
         }
-        if (sec >= 10 && sec <= 60) {
-          timeRem.textContent = `Time Remaining: 0${mins}:${--sec}`;
-        }
-      }, 1000);
-    }
+      }
+      if (sec >= 10 && sec <= 60) {
+        timeRem.textContent = `Time Remaining: 0${mins}:${--sec}`;
+      }
+    }, 1000);
+  }
 
-    if (intTxt > 600 && intTxt < 6000) {
-      mins = Math.floor (intTxt / 60);
-      sec = intTxt - mins * 60;
-      interv = setInterval (function () {
-        if (sec > 0 && sec < 10) {
-          if (sec > 0)
-            timeRem.textContent = `Time Remaining: ${mins}:0${--sec}`;
-          if (sec === 0) {
-            if (mins > 0) {
-              mins--;
-              sec += 60;
-            }
-            timeRem.textContent = `Time Remaining: 0${mins}:0${sec}`;
+  if (intTxt > 600 && intTxt < 6000) {
+    mins = Math.floor (intTxt / 60);
+    sec = intTxt - mins * 60;
+    interv = setInterval (function () {
+      if (sec > 0 && sec < 10) {
+        if (sec > 0) timeRem.textContent = `Time Remaining: ${mins}:0${--sec}`;
+        if (sec === 0) {
+          if (mins > 0) {
+            mins--;
+            sec += 60;
           }
+          timeRem.textContent = `Time Remaining: 0${mins}:0${sec}`;
         }
-        if (sec >= 10 && sec <= 60) {
-          timeRem.textContent = `Time Remaining: 0${mins}:${--sec}`;
-        }
-      }, 1000);
-    }
+      }
+      if (sec >= 10 && sec <= 60) {
+        timeRem.textContent = `Time Remaining: 0${mins}:${--sec}`;
+      }
+    }, 1000);
+  }
 
-    if (intTxt > 6000) {
-      timeRem.textContent = ` Sorry Its Is Too Big, Follow Up Our Next Version 😉`;
-    }
-  });
+  if (intTxt > 6000) {
+    timeRem.textContent = ` Sorry Its Is Too Big, Follow Up Our Next Version 😉`;
+  }
 }
 
 // DO NOT EDIT BELOW HERE
