cbb98a3c5a803a8911d622ab4e1a0d9544998b1c
osagiestar <okoedooc@yahoo.com>
2020-08-01 03:49:32 +0100

    fcompleted task

--- Week-3/Homework/mandatory/1-alarmclock/alarmclock.js
+++ Week-3/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,30 @@
-function setAlarm() {}
+function setAlarm() {
+  let alarmCounter = document.querySelector("#timeRemaining");
+  let bodyClass = document.querySelector(".centre");
+  setAlarmTime = document.querySelector("#alarmSet");
+  let inputValue = setAlarmTime.value;
+
+  let minutes = Math.floor(inputValue / 60);
+  let seconds = inputValue % 60;
+
+  let interval = setInterval(() => {
+    if (minutes > 0 && seconds === 0) {
+      minutes--;
+      seconds = 59;
+    }
+
+    if (seconds === 0) {
+      clearInterval(interval);
+      audio.play();
+      bodyClass.style.backgroundColor = "#B5AEA8";
+    }
+
+    alarmCounter.textContent = `Time Remaining: ${minutes} : ${seconds}`;
+    seconds--;
+  }, 1000);
+
+  audio.pause();
+}
 
 // DO NOT EDIT BELOW HERE
 
