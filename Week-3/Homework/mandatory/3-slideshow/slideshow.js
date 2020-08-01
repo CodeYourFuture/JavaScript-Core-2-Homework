0f878342865944948438a98b87829f4f606bcc70
iggajewski <64140239+iggajewski@users.noreply.github.com>
2020-08-01 01:15:22 +0100

    completed exercise 3; some fixes to in-class

--- Week-3/Homework/mandatory/3-slideshow/slideshow.js
+++ Week-3/Homework/mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,76 @@
-// Write your code here
+var image;
+var auto;
+var index = 0;
+var autoSwitchedOn = false;
+
+function moveBack() {
+    if(index <= 0) {
+        index = imageFilenames.length - 1;
+        image.src = "images/" + imageFilenames[index];
+    }
+    else {
+        image.src = "images/" + imageFilenames[--index];
+    }
+}
+
+function moveForward() {
+    if(index >= imageFilenames.length - 1) {
+        index = 0;
+        image.src = "images/" + imageFilenames[index];
+    }
+    else {
+        image.src = "images/" + imageFilenames[++index];
+    }
+}
+
+function autoMoveBack() {
+    if(autoSwitchedOn) {
+        return;
+    }
+    else {
+        autoSwitchedOn = true;
+        moveBack();
+        auto = setInterval(moveBack, 5000);
+    }
+}
+
+function autoMoveForward() {
+    if(autoSwitchedOn) {
+        return;
+    }
+    else {
+        autoSwitchedOn = true;
+        moveForward();
+        auto = setInterval(moveForward, 5000);
+    }
+}
+
+function stopAutoMovement() {
+    autoSwitchedOn = false;
+    clearInterval(auto);
+}
+
+function setup() {
+    image =  document.getElementById("slide-image");
+    image.src = "images/" + imageFilenames[index];   
+
+    document.getElementById("back").addEventListener("click", moveBack);
+    document.getElementById("forward").addEventListener("click", moveForward);
+    document.getElementById("auto-back").addEventListener("click", autoMoveBack);
+    document.getElementById("auto-forward").addEventListener("click",  autoMoveForward);
+    document.getElementById("stop").addEventListener("click", stopAutoMovement);
+}
+
+window.onload = setup;
+
+/* IMAGE DATABASE: */
+
+var imageFilenames = [
+    "pexels-photo-236606.jpeg",
+    "pexels-photo-1056251.jpeg",
+    "pexels-photo-1543793.jpeg",
+    "pexels-photo-1787414.jpeg",
+    "pexels-photo-2558605.jpeg",
+    "pexels-photo-2643812.jpeg",
+    "pexels-photo-2870353.jpeg"
+];
--- Week-3/InClass/DOM-practice/main.js
+++ Week-3/InClass/DOM-practice/main.js
@@ -18,9 +18,9 @@ for (let i = 0; i < sections.length; ++i) {
 // Hint: look at the CSS to see if there are any classes already written which you can use.
 
 
-let headerEls = document.querySelectorAll("img");
-for (let i = 0; i < headerEls.length; ++i) {
-    headerEls[i].className += " content-title";
+let images = document.querySelectorAll("img");
+for (let i = 0; i < images.length; ++i) {
+    images[i].className += " content-title";
 }
 
 
@@ -28,3 +28,15 @@ for (let i = 0; i < headerEls.length; ++i) {
 // Task 3
 
 // Google the date of birth and death of each of the people on the page. Without changing any of the HTML or CSS, add this in a paragraph to the end of their <section>.
+
+let firstParagraph = document.createElement("p");
+firstParagraph.innerHTML = "Grace Hopper was born on December 9, 1906, and died on January 1, 1992, at the age of 85.";
+sections[0].appendChild(firstParagraph);
+
+let secondParagraph = document.createElement("p");
+secondParagraph.innerHTML = "Katherine Johnson was born on August 26, 1918, and died on February 24, 2020, at the age of 101.";
+sections[1].appendChild(secondParagraph);
+
+let thirdParagraph = document.createElement("p");
+thirdParagraph.innerHTML = "Ada Lovelace was born on December 10, 1815, and died on November 27, 1852, at the age of 36.";
+sections[2].appendChild(thirdParagraph);
