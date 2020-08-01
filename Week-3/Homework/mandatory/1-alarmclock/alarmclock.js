13f0ff4605e818de6425a1bb2bf20a9a7482b736
Abdennour Hachemi <nouri89@hotmail.com>
2020-08-01 23:48:59 +0100

    Soved

--- Week-3/Homework/mandatory/1-alarmclock/alarmclock.js
+++ Week-3/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,25 +1,66 @@
-function setAlarm() {}
+function setAlarm() {
+	let timeSet = document.querySelector('#alarmSet').value;
+	function countDown() {
+		let minutes;
+		let seconds;
+		if (timeSet >= 10 && timeSet < 60) {
+			(minutes = '00'), (seconds = '' + timeSet);
+			document.querySelector('#timeRemaining').innerText = 'Time Remaining:00:' + timeSet;
+		}
+		if (timeSet < 10) {
+			(minutes = '00'), (seconds = '0' + timeSet);
+			document.querySelector('#timeRemaining').innerText = 'Time Remaining:00:0' + timeSet;
+		} else if (timeSet > 59) {
+			let testMinutes = Math.floor(timeSet / 60);
+			let testSeconds = timeSet % 60;
+
+			if (testMinutes < 10 && testSeconds < 10) {
+				document.querySelector('#timeRemaining').innerText =
+					'Time Remaining: 0' + Math.floor(timeSet / 60) + ':0' + timeSet % 60;
+			}
+			if (testMinutes < 10 && testSeconds > 10) {
+				document.querySelector('#timeRemaining').innerText =
+					'Time Remaining: 0' + Math.floor(timeSet / 60) + ':' + timeSet % 60;
+			}
+			if (testMinutes > 10 && testSeconds < 10) {
+				document.querySelector('#timeRemaining').innerText =
+					'Time Remaining:' + Math.floor(timeSet / 60) + ':0' + timeSet % 60;
+			}
+			if (testMinutes > 10 && testSeconds > 10) {
+				document.querySelector('#timeRemaining').innerText =
+					'Time Remaining:' + Math.floor(timeSet / 60) + ':' + timeSet % 60;
+			}
+		}
+
+		if (timeSet === 0) {
+			clearInterval(myaAlarm);
+			playAlarm();
+			document.body.style.backgroundColor = 'aqua';
+		}
+		timeSet--;
+	}
+}
 
 // DO NOT EDIT BELOW HERE
 
-var audio = new Audio("alarmsound.mp3");
+var audio = new Audio('alarmsound.mp3');
 
 function setup() {
-  document.getElementById("set").addEventListener("click", () => {
-    setAlarm();
-  });
+	document.getElementById('set').addEventListener('click', () => {
+		setAlarm();
+	});
 
-  document.getElementById("stop").addEventListener("click", () => {
-    pauseAlarm();
-  });
+	document.getElementById('stop').addEventListener('click', () => {
+		pauseAlarm();
+	});
 }
 
 function playAlarm() {
-  audio.play();
+	audio.play();
 }
 
 function pauseAlarm() {
-  audio.pause();
+	audio.pause();
 }
 
 window.onload = setup;
