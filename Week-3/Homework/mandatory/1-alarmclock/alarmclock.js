a371770e3c36aceefd494994e1d3af3dd2cc7027
Jacques Vertommen <jacquesvertommen@gmail.com>
2020-08-14 23:25:05 +0100

    generator quote

--- Week-3/Homework/mandatory/1-alarmclock/alarmclock.js
+++ Week-3/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,36 @@
-function setAlarm() {}
+
+
+
+function setAlarm() {
+  //Access to the DOM
+  let timeRemaining = document.getElementById('timeRemaining');
+  let alarmSet = document.getElementById('alarmSet');
+
+  let inputNumber = alarmSet.value;
+  let minutes = Math.floor(inputNumber/60);
+  let seconds = inputNumber % 60;
+    
+  let interval = setInterval(() => {
+    if (minutes > 0 && seconds === 0) {
+      minutes--;
+      seconds = 59;
+    }
+
+    if (seconds === 0) {
+      clearInterval(interval);
+      audio.play();
+      let bgClock = document.body.style.backgroundColor = "magenta";
+      bgClock.reset();
+    }
+
+    timeRemaining.innerHTML = 'Time Remaining: ' + minutes + ':' + seconds;
+    seconds--;
+  }, 1000);
+
+  audio.pause();
+}
+    
+
 
 // DO NOT EDIT BELOW HERE
 
