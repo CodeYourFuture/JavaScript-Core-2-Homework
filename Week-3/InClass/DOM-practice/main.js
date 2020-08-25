f4f02821063a1423dc0e3a56cc8f0a6ef06556e4
Ali Haider <alihaiderx736@gmail.com>
2020-08-25 22:27:02 +0100

    In Class DOM Practice is Completed.

--- Week-3/InClass/DOM-practice/main.js
+++ Week-3/InClass/DOM-practice/main.js
@@ -3,7 +3,10 @@ console.log("Testing JS file loaded!")
 // Task 1
 
 // Without changing any of the HTML or CSS, update the <section> tags so that they have white backgrounds.
-
+  let getSection = document.querySelectorAll("section");
+   for(var i=0;i<getSection.length;i++){
+       getSection[i].style.backgroundColor ="white";
+   }
 
 
 
@@ -12,6 +15,11 @@ console.log("Testing JS file loaded!")
 // Task 2
 
 // Without changing any of the HTML or CSS, update the images on the page so that they are all centered.
+let getImages =document.querySelectorAll("img");
+for(var i=0;i<getImages.length;i++){
+    getImages[i].className = "content-title";
+}
+
 
 // Hint: look at the CSS to see if there are any classes already written which you can use.
 
@@ -23,3 +31,12 @@ console.log("Testing JS file loaded!")
 // Task 3
 
 // Google the date of birth and death of each of the people on the page. Without changing any of the HTML or CSS, add this in a paragraph to the end of their <section>.
+  let dateOfBirth = ["9-12-1906","26-8-1918","10-12-1815"];
+let dateOfDeath = ["1-1-1992", "24-2-2020", "27-11-2020"];
+
+
+for(var i=0;i<getSection.length;i++){
+ let newP = document.createElement("p");
+ newP.textContent = "Date of Birth is "+dateOfBirth[i]+" and Date of Death is "+dateOfDeath[i];
+ getSection[i].appendChild(newP);
+}
