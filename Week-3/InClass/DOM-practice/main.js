619f9d4e3dcd194f28c7338d7004919c15ce7f46
domenico.cyf <domenico.cyf@gmail.com>
2021-03-22 18:24:25 +0000

    Update main.js

--- Week-3/InClass/DOM-practice/main.js
+++ Week-3/InClass/DOM-practice/main.js
@@ -5,10 +5,16 @@ console.log("Testing JS file loaded!")
 // Without changing any of the HTML or CSS, update the <section> tags so that they have white backgrounds.
 
 
+function sum(a,b){
+    
+    return a + b;
 
+}
 
+console.log(sum(12,1));
 
-
+let sum2 = (a,b) => a+b;
+console.log(sum2(2,3))
 // Task 2
 
 // Without changing any of the HTML or CSS, update the images on the page so that they are all centered.
